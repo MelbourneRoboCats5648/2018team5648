b91158a59807d2635c561f3d5254d7fccbb3c918
Milorad Cerovac <mcerovac@swin.edu.au>
2018-01-28 12:42:08 +1100

    Updated class to allow camera use. Currently working from Logitech joystick.

--- src/org/usfirst/frc/team5648/robot/Robot.java
+++ src/org/usfirst/frc/team5648/robot/Robot.java
@@ -4,121 +4,7 @@
 ///* must be accompanied by the FIRST BSD license file in the root directory of */
 ///* the project.                                                               */
 ///*----------------------------------------------------------------------------*/
-//
-//package org.usfirst.frc.team5648.robot;
-//
-//import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.command.Command;
-//import edu.wpi.first.wpilibj.command.Scheduler;
-//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import org.usfirst.frc.team5648.robot.commands.ExampleCommand;
-//import org.usfirst.frc.team5648.robot.subsystems.ExampleSubsystem;
-//
-///**
-// * The VM is configured to automatically run this class, and to call the
-// * functions corresponding to each mode, as described in the TimedRobot
-// * documentation. If you change the name of this class or the package after
-// * creating this project, you must also update the build.properties file in the
-// * project.
-// */
-//public class Robot extends TimedRobot {
-//	public static final ExampleSubsystem kExampleSubsystem
-//			= new ExampleSubsystem();
-//	public static OI m_oi;
-//
-//	Command m_autonomousCommand;
-//	SendableChooser<Command> m_chooser = new SendableChooser<>();
-//
-//	/**
-//	 * This function is run when the robot is first started up and should be
-//	 * used for any initialization code.
-//	 */
-//	@Override
-//	public void robotInit() {
-//		m_oi = new OI();
-//		m_chooser.addDefault("Default Auto", new ExampleCommand());
-//		// chooser.addObject("My Auto", new MyAutoCommand());
-//		SmartDashboard.putData("Auto mode", m_chooser);
-//	}
-//
-//	/**
-//	 * This function is called once each time the robot enters Disabled mode.
-//	 * You can use it to reset any subsystem information you want to clear when
-//	 * the robot is disabled.
-//	 */
-//	@Override
-//	public void disabledInit() {
-//
-//	}
-//
-//	@Override
-//	public void disabledPeriodic() {
-//		Scheduler.getInstance().run();
-//	}
-//
-//	/**
-//	 * This autonomous (along with the chooser code above) shows how to select
-//	 * between different autonomous modes using the dashboard. The sendable
-//	 * chooser code works with the Java SmartDashboard. If you prefer the
-//	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-//	 * getString code to get the auto name from the text box below the Gyro
-//	 *
-//	 * <p>You can add additional auto modes by adding additional commands to the
-//	 * chooser code above (like the commented example) or additional comparisons
-//	 * to the switch structure below with additional strings & commands.
-//	 */
-//	@Override
-//	public void autonomousInit() {
-//		m_autonomousCommand = m_chooser.getSelected();
-//
-//		/*
-//		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-//		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-//		 * = new MyAutoCommand(); break; case "Default Auto": default:
-//		 * autonomousCommand = new ExampleCommand(); break; }
-//		 */
-//
-//		// schedule the autonomous command (example)
-//		if (m_autonomousCommand != null) {
-//			m_autonomousCommand.start();
-//		}
-//	}
-//
-//	/**
-//	 * This function is called periodically during autonomous.
-//	 */
-//	@Override
-//	public void autonomousPeriodic() {
-//		Scheduler.getInstance().run();
-//	}
-//
-//	@Override
-//	public void teleopInit() {
-//		// This makes sure that the autonomous stops running when
-//		// teleop starts running. If you want the autonomous to
-//		// continue until interrupted by another command, remove
-//		// this line or comment it out.
-//		if (m_autonomousCommand != null) {
-//			m_autonomousCommand.cancel();
-//		}
-//	}
-//
-//	/**
-//	 * This function is called periodically during operator control.
-//	 */
-//	@Override
-//	public void teleopPeriodic() {
-//		Scheduler.getInstance().run();
-//	}
-//
-//	/**
-//	 * This function is called periodically during test mode.
-//	 */
-//	@Override
-//	public void testPeriodic() {
-//	}
-//}
+
 
 package org.usfirst.frc.team5648.robot;
 
@@ -128,19 +14,17 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalOutput;
-//import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.MotorSafety;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Relay.Value;
-//import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
-//import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -174,6 +58,12 @@ public class Robot extends IterativeRobot {
 
 	private static final String distanceKey = "Distance Traveled in Auto mode";
 	private static final String autoPeriodKey = "Number of Auto mode periods we have driven for";
+	private static final String driveRotation = "DriveRotation";
+	private static final String driveMoveValue = "DriveMove";
+	private static final String xValue = "xValue";
+	private static final String yValue = "yValue";
+	private static final String throttle = "throttle";
+	private static final String scaledThrottle = "scaledThrottle";
 	
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
@@ -194,6 +84,7 @@ public class Robot extends IterativeRobot {
 	// how many AutoPeriodic periods have executed that we have driven forward for
 	int autoAccumulatedPeriods;
 	Spark climbingMechanism;
+	private NetworkTableInstance ntInstance;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -257,20 +148,29 @@ public class Robot extends IterativeRobot {
 		autoAccumulatedDistance = 0.0;
 		autoAccumulatedPeriods = 0;
 		
-		// create joystick table
-//		joystickTable = NetworkTable.getTable("Joystick Values");
-//		motorTable = NetworkTable.getTable("Motor Input");
-		
-		// initialise the Joystick/Motor input tables
-//		motorTable.putNumber(distanceKey, autoAccumulatedDistance);
-//		motorTable.putNumber(autoPeriodKey, autoAccumulatedPeriods); 		
-//		motorTable.putNumber("driveRotation", 0);
-//		motorTable.putNumber("driveMoveValue", 0);	
-//		joystickTable.putNumber("xValue", 0);
-//		joystickTable.putNumber("yValue", 0);
-//		joystickTable.putNumber("throttle", 0);
-//		joystickTable.putNumber("scaledThrottle", 0.0);
-//		joystickTable.putBoolean("climbing motor trigger", false);
+		try {
+			// create debug tables
+			ntInstance = NetworkTableInstance.getDefault();
+			NetworkTable debugTable = ntInstance.getTable("Debug");
+			joystickTable = debugTable.getSubTable("Joystick Values");
+			motorTable = debugTable.getSubTable("Motor Input");
+			
+			// initialise the Joystick/Motor input tables
+			motorTable.getEntry(distanceKey).setNumber(autoAccumulatedDistance);
+			motorTable.getEntry(autoPeriodKey).setNumber(autoAccumulatedPeriods); 		
+			motorTable.getEntry(driveRotation).setNumber(0); 
+			motorTable.getEntry(driveMoveValue).setNumber(0);	
+			
+			joystickTable.getEntry(xValue).setNumber(0);
+			joystickTable.getEntry(yValue).setNumber(0);
+			joystickTable.getEntry(throttle).setNumber(0);
+			joystickTable.getEntry(scaledThrottle).setNumber(0.0);
+			joystickTable.getEntry("climbing motor trigger").setBoolean(false);
+		}
+		catch (Exception e)
+		{
+			System.err.println("Issue creating network table entries, continuing init.");	
+		}
 	}
 
 	/**
@@ -327,8 +227,8 @@ public class Robot extends IterativeRobot {
 			slideRelayRelayPort.set(Value.kForward);
 		}
 		
-//		motorTable.putNumber(distanceKey, autoAccumulatedDistance);
-//		motorTable.putNumber(autoPeriodKey, autoAccumulatedPeriods);
+     	motorTable.getEntry(distanceKey).setNumber(autoAccumulatedDistance);
+		motorTable.getEntry(autoPeriodKey).setNumber(autoAccumulatedPeriods); 
 		
 	}
 
@@ -353,20 +253,21 @@ public class Robot extends IterativeRobot {
 		
 		boolean trigger = joystick.getTrigger();
 		
-		// update joystick values in Network Tables for display in the dashboard		
-//		joystickTable.putNumber("xValue", xValue);
-//		joystickTable.putNumber("yValue", yValue);
-//		joystickTable.putNumber("throttle", throttle);
-//		joystickTable.putBoolean("climbing motor trigger", trigger);
-//		joystickTable.putNumber("scaledThrottle", scaledThrottle);
+		// update joystick values in Network Tables for display in the dashboard
+		// note that new values will not show in the Dashboard unless it is restarted
+		joystickTable.getEntry(Robot.xValue).setNumber(xValue);
+		joystickTable.getEntry(Robot.yValue).setNumber(yValue);
+		joystickTable.getEntry(Robot.throttle).setNumber(throttle);
+		joystickTable.getEntry("climbing motor trigger").setBoolean(trigger);
+		joystickTable.getEntry(Robot.scaledThrottle).setNumber(scaledThrottle);
 		
 		// calculate drive values - modify power by scaled throttle
 		double driveRotatation = -xValue*scaledThrottle;
 		double driveMoveValue = yValue*scaledThrottle;
 		
 		// update motor drive values in Network Tables for display in the dashboard		
-//		motorTable.putNumber("driveRotation", driveRotatation);
-//		motorTable.putNumber("driveMoveValue", driveMoveValue);		
+		motorTable.getEntry(Robot.driveRotation).setNumber(driveRotatation);
+		motorTable.getEntry(Robot.driveMoveValue).setNumber(driveMoveValue);		
 		
 		// send drive values to motor
 		DriveMotors(driveMoveValue, driveRotatation);
